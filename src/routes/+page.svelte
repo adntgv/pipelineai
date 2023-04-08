486f2290b85769360b8d7d4271cb99218e9b181b
Aidyn Torgayev <aidyn.torgayev@gmail.com>
2023-04-08 17:26:54 +0600

    pipeline name edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,11 @@
 	import ApiKeyDialog from './apiKeyDialog.svelte';
 	import Block from './block.svelte';
 	import type { BlockHandler, RunBlock } from './types';
-	import Button, { Group } from '@smui/button';
+	import Button, { Group, Icon } from '@smui/button';
+	import Drawer, { AppContent, Content } from '@smui/drawer';
+	import List, { Item, Text } from '@smui/list';
+	import Textfield from '@smui/textfield';
+	import IconButton from '@smui/icon-button';
 
 	let promptHandler: BlockHandler = async (prompt: string, input: string): Promise<string> => {
 		const data = await fetch('http://localhost:5173/api/prompt', {
@@ -159,35 +163,126 @@
 			return index != target;
 		});
 	};
+
+	let pipelines = [
+		{
+			name: 'Default',
+			blocks: [
+				{
+					prompt: defaultPrompt,
+					input: '',
+					output: '',
+					handler: promptHandler,
+					active: false,
+					type: 'prompt'
+				}
+			]
+		}
+	];
+
+	const addPipeline = () => {
+		pipelines.push({
+			name: 'New Pipeline',
+			blocks: [
+				{
+					prompt: defaultPrompt,
+					input: '',
+					output: '',
+					handler: promptHandler,
+					active: false,
+					type: 'prompt'
+				}
+			]
+		});
+
+		pipelines = pipelines;
+	};
+
+	let editingPipelineName = -1;
 </script>
 
-<div style="display: flex; justify-content: stretch;">
-	<Group style="margin: 0 auto;" variant="outlined">
-		<!-- <ApiKeyDialog /> -->
+<div class="drawer-container">
+	<Drawer>
+		<Content>
+			<Button variant="outlined" on:click={() => addPipeline()}>Add Pipeline</Button>
+			<List>
+				{#each pipelines as pipeline, index}
+					<Item href="javascript:void(0)" on:click={() => (blocks = pipeline.blocks)}>
+						{#if editingPipelineName === index}
+							<Textfield style="width: 100%;" bind:value={pipeline.name} label="name" />
+							<Button on:click={() => (editingPipelineName = -1)}>save</Button>
+						{:else}
+							<Text>{pipeline.name}</Text>
+							<Button on:click={() => (editingPipelineName = index)}>rename</Button>
+						{/if}
+					</Item>
+				{/each}
+			</List>
+		</Content>
+	</Drawer>
 
-		<Button variant="outlined" on:click={() => runPipeline()}>Run Pipeline</Button>
-		<Button variant="outlined" on:click={() => stopPipeline()}>Stop Pipeline</Button>
-	</Group>
-</div>
-<LayoutGrid>
-	{#each blocks as block, index}
-		<Cell span={2} />
-
-		<Cell span={8}>
-			<Button
-				on:click={() => {
-					remove(index);
-				}}>remove</Button
+	<AppContent class="app-content">
+		<main class="main-content">
+			<div
+				style="display: flex; justify-content: space-between; align-items: center; padding: 10px;
+						position: fixed; top: 0;  z-index: 1000;
+					"
 			>
-			<Block {block} />
-			<hr />
-		</Cell>
-		<Cell span={2} />
-	{/each}
-	<Cell span={1}>
-		<Button on:click={() => addBlock('prompt')}>+ Prompt</Button>
-		<Button on:click={() => addBlock('google')}>+ google</Button>
-		<Button on:click={() => addBlock('crawl')}>+ crawl</Button>
-		<Button on:click={() => addBlock('imagine')}>+ imagine</Button>
-	</Cell>
-</LayoutGrid>
+				<Group style="margin: 0 auto;" variant="outlined">
+					<!-- <ApiKeyDialog /> -->
+					<Button variant="outlined" on:click={() => runPipeline()}>Run Pipeline</Button>
+					<Button variant="outlined" on:click={() => stopPipeline()}>Stop Pipeline</Button>
+				</Group>
+				<Group style="margin: 0 auto;" variant="outlined">
+					<Button variant="outlined" on:click={() => addBlock('prompt')}>+ Prompt</Button>
+					<Button variant="outlined" on:click={() => addBlock('google')}>+ Google</Button>
+					<Button variant="outlined" on:click={() => addBlock('crawl')}>+ Crawl</Button>
+					<Button variant="outlined" on:click={() => addBlock('imagine')}>+ Imagine</Button>
+				</Group>
+			</div>
+			<LayoutGrid>
+				{#each blocks as block, index}
+					<Cell span={2} />
+
+					<Cell span={8}>
+						<Button
+							on:click={() => {
+								remove(index);
+							}}>remove</Button
+						>
+						<Block {block} />
+						<hr />
+					</Cell>
+					<Cell span={2} />
+				{/each}
+			</LayoutGrid>
+		</main>
+	</AppContent>
+</div>
+
+<style>
+	/* These classes are only needed because the
+	  drawer is in a container on the page. */
+	.drawer-container {
+		position: relative;
+		display: flex;
+		height: 98vh;
+		border: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+		overflow: hidden;
+		z-index: 0;
+	}
+
+	* :global(.app-content) {
+		flex: auto;
+		overflow: auto;
+		position: relative;
+		flex-grow: 1;
+	}
+
+	.main-content {
+		overflow: auto;
+		padding: 16px;
+		height: 100%;
+		box-sizing: border-box;
+	}
+</style>
